cce299406dedcc2594f74a04e5c1be05da68f345
anilchejarla <anilchejarla205@gmail.com>
2017-08-04 22:50:15 +0530

    this is the multiplication...

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+ass Addition
+{
+static int a=78999, b=5117,c=mlti;
+public static void main(String[] ani)
+{
+mlti=a*b;
+System.out.println("The multiplication of a&b are stored in the variable c....");
+System.out.println("So the value of c is...:"+mlti);
+}
+}
+i
